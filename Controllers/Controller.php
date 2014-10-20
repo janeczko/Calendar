12e948702d7b134530f642d541cd8e4c949295ee
janeczko <janeczko@webvalley.cz>
2014-10-20 22:11:48 +0200

    update

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -1,8 +1,52 @@
 <?php
 
-namespace Controllers;
-
 class Controller
 {
+    protected $parameters;
+    protected $title;
+    protected $renderData;
+    protected $db;
 
+    public function __construct()
+    {
+        $this->renderData = [];
+        $this->db = null;   // TODO - Connect database
+        $this->title = 'Kalendář';
+    }
+
+    public function initialize()
+    {
+
+    }
+
+    public function renderView($view)
+    {
+        extract($this->renderData);
+        $title = $this->title;
+
+        require $this->viewPath($view);
+    }
+
+    protected function viewPath($view)
+    {
+        return 'Views/' . $view . '.phtml';
+    }
+
+    protected function redirect($url)
+    {
+        header('Location: /' . $url);
+        header('Connection: close');
+        exit;
+    }
+
+    public function renderAction($action)
+    {
+        $this->initialize();
+        $this->$action();
+    }
+
+    public function setParameters($paremeters)
+    {
+        $this->parameters = $paremeters;
+    }
 } 
--- index.php
+++ index.php
@@ -1,2 +1,52 @@
 <?php
 
+mb_internal_encoding('utf-8');
+
+function classLoad($class)
+{
+    if (preg_match('/Controller$/', $class))
+        require 'Controllers/' . $class . '.php';
+    else
+        require 'Models/' . $class . '.php';
+}
+
+spl_autoload_register('classLoad');
+
+class Router
+{
+    protected $urlParameters;
+    protected $controller;
+    protected $action;
+
+    public function __construct()
+    {
+        $this->urlParameters = [];
+
+        foreach ($_GET as $key => $value)
+            $this->urlParameters[] = [$key => $value];
+    }
+
+    public function route()
+    {
+        if (isset($this->urlParameters[0]))
+        {
+            $name = str_replace(' ', '', ucwords(str_replace('-', ' ', key(array_shift($this->urlParameters))))) . 'Controller';
+            $this->controller = new $name();
+
+            if (!isset($this->urlParameters[0]))
+            {
+                $this->controller = new ErrorController();
+            }
+            else
+            {
+                $this->action = array_shift($this->urlParameters);
+                $this->controller->setParameters($this->urlParameters);
+            }
+        }
+        else
+            $this->controller = new HomeController();
+    }
+}
+
+$router = new Router();
+$router->route();
